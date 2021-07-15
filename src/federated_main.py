7ddb59185f73737773037b59f67d800f6cdba070
ongzh <66582818+ongzh@users.noreply.github.com>
2021-07-15 14:20:43 +0800

    added resnet18

---
+++ src/federated_main.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+
+import os
+import copy
+import time
+import pickle
+import numpy as np
+from tqdm import tqdm
+
+import torch
+import gc
+from tensorboardX import SummaryWriter
+
+from options import args_parser
+from update import LocalUpdate, test_inference
+from models import MLP, CNNMnist, CNNFashion_Mnist, CNNCifar, VGG
+from utils import get_dataset, average_weights, exp_details
+import torchvision.models as models
+
+if __name__ == '__main__':
+    start_time = time.time()
+
+    # define paths
+    path_project = os.path.abspath('..')
+    logger = SummaryWriter('../logs')
+
+    args = args_parser()
+    exp_details(args)
+
+    if args.gpu:
+        # if args.gpu_id:
+        torch.cuda.set_device(int(args.gpu))
+    device = 'cuda' if args.gpu else 'cpu'
+
+    # load dataset and user groups
+    train_dataset, test_dataset, user_groups = get_dataset(args)
+
+    # BUILD MODEL
+    if args.model == 'cnn':
+        # Convolutional neural netork
+        if args.dataset == 'mnist':
+            global_model = CNNMnist(args=args)
+        elif args.dataset == 'fmnist':
+            global_model = CNNFashion_Mnist(args=args)
+        elif args.dataset == 'cifar':
+            global_model = CNNCifar(args=args)
+
+    elif args.model == 'mlp':
+        # Multi-layer preceptron
+        img_size = train_dataset[0][0].shape
+        len_in = 1
+        for x in img_size:
+            len_in *= x
+            global_model = MLP(dim_in=len_in, dim_hidden=64,
+                               dim_out=args.num_classes)
+    elif args.model == 'vgg':
+        if args.dataset == 'cifar' and args.pretrained:
+            global_model = models.vgg16(pretrained=True)
+            # change the number of classes
+            global_model.classifier[6].out_features = 10
+            # freeze convolution weights
+            for param in global_model.features.parameters():
+                param.requires_grad = False
+        elif args.dataset == 'cifar':
+            global_model = VGG(args=args)
+        else:
+            exit(args.dataset + ' with ' + args.model + ' not supported')
+
+    elif args.model == "resnet18":
+        global_model = models.resnet18(pretrained=True)
+        
+        
+    else:
+        exit('Error: unrecognized model')
+
+    # Set the model to train and send it to device.
+    global_model.to(device)
+    global_model.train()
+    print(global_model)
+
+    # copy weights
+    global_weights = global_model.state_dict()
+
+    # Training
+    train_loss, train_accuracy = [], []
+    val_acc_list, net_list = [], []
+    cv_loss, cv_acc = [], []
+    print_every = 2
+    val_loss_pre, counter = 0, 0
+
+    # Test each round
+    test_acc_list = []
+
+    for epoch in tqdm(range(args.epochs)):
+        local_weights, local_losses = [], []
+        print(f'\n | Global Training Round : {epoch+1} |\n')
+
+        global_model.train()
+        m = max(int(args.frac * args.num_users), 1)
+        idxs_users = np.random.choice(range(args.num_users), m, replace=False)
+
+        for idx in idxs_users:
+            local_model = LocalUpdate(args=args, dataset=train_dataset,
+                                      idxs=user_groups[idx], logger=logger)
+            w, loss = local_model.update_weights(
+                model=copy.deepcopy(global_model), global_round=epoch)
+            local_weights.append(copy.deepcopy(w))
+            local_losses.append(copy.deepcopy(loss))
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        # update global weights
+        global_weights = average_weights(local_weights)
+
+        # update global weights
+        global_model.load_state_dict(global_weights)
+
+        loss_avg = sum(local_losses) / len(local_losses)
+        train_loss.append(loss_avg)
+
+        # Calculate avg training accuracy over all users at every epoch
+        list_acc, list_loss = [], []
+        global_model.eval()
+
+        for c in range(args.num_users):
+            local_model = LocalUpdate(args=args, dataset=train_dataset,
+                                      idxs=user_groups[idx], logger=logger)
+            acc, loss = local_model.inference(model=global_model)
+            list_acc.append(acc)
+            list_loss.append(loss)
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        train_accuracy.append(sum(list_acc)/len(list_acc))
+
+        round_test_acc, round_test_loss = test_inference(
+            args, global_model, test_dataset)
+        test_acc_list.append(round_test_acc)
+
+        # print global training loss after every 'i' rounds
+        if (epoch+1) % print_every == 0:
+            print(f' \nAvg Training Stats after {epoch+1} global rounds:')
+            print(f'Training Loss : {np.mean(np.array(train_loss))}')
+            print('Train Accuracy: {:.2f}% \n'.format(100*train_accuracy[-1]))
+            print('Test Accuracy at round ' + str(epoch+1) +
+                  ': {:.2f}% \n'.format(100*round_test_acc))
+
+    # Test inference after completion of training
+    test_acc, test_loss = test_inference(args, global_model, test_dataset)
+
+    print(f' \n Results after {args.epochs} global rounds of training:')
+    print("|---- Avg Train Accuracy: {:.2f}%".format(100*train_accuracy[-1]))
+    print("|---- Test Accuracy: {:.2f}%".format(100*test_acc))
+
+    # Saving the objects train_loss and train_accuracy:
+    file_name = '../save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'.\
+        format(args.dataset, args.model, args.epochs, args.frac, args.iid,
+               args.local_ep, args.local_bs)
+
+    with open(file_name, 'wb') as f:
+        pickle.dump([train_loss, train_accuracy], f)
+
+    print('\n Total Run Time: {0:0.4f}'.format(time.time()-start_time))
+
+    # save results to csv
+    res = np.asarray([test_acc_list])
+    res_name = '../save/csvResults/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_LR[{}].csv'. \
+        format(args.dataset, args.model, args.epochs, args.frac, args.iid,
+               args.local_ep, args.local_bs, args.lr)
+    np.savetxt(res_name, res, delimiter=",")
+
+    # PLOTTING (optional)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    matplotlib.use('Agg')
+
+    # # Plot Loss curve
+    # plt.figure()
+    # plt.title('Training Loss vs Communication rounds')
+    # plt.plot(range(len(train_loss)), train_loss, color='r')
+    # plt.ylabel('Training loss')
+    # plt.xlabel('Communication Rounds')
+    # plt.savefig('../save/fed_{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_loss.png'.
+    #             format(args.dataset, args.model, args.epochs, args.frac,
+    #                     args.iid, args.local_ep, args.local_bs))
+
+    # # Plot Average Accuracy vs Communication rounds
+    # plt.figure()
+    # plt.title('Average Accuracy vs Communication rounds')
+    # plt.plot(range(len(train_accuracy)), train_accuracy , color='k')
+    # plt.ylabel('Average Accuracy')
+    # plt.xlabel('Communication Rounds')
+    # plt.savefig('../save/fed_{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_acc.png'.
+    #              format(args.dataset, args.model, args.epochs, args.frac,
+    #                     args.iid, args.local_ep, args.local_bs))
+
+    # Plot Test Accuracy vs Communication rounds
+    plt.figure()
+    plt.title('Test Accuracy vs Communication rounds')
+    plt.plot(range(len(train_accuracy)), test_acc_list, color='k')
+    plt.ylabel('Test Accuracy')
+    plt.xlabel('Communication Rounds')
+    plt.ylim([0.1, 1])
+    plt.savefig('../save/fed_{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}]_LR[{}]_test_acc.png'.
+                format(args.dataset, args.model, args.epochs, args.frac,
+                       args.iid, args.local_ep, args.local_bs, args.lr))
---
+++ src/models.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Python version: 3.6
+
+from torch import nn
+import torch
+import torch.nn.functional as F
+
+
+class MLP(nn.Module):
+    def __init__(self, dim_in, dim_hidden, dim_out):
+        super(MLP, self).__init__()
+        self.layer_input = nn.Linear(dim_in, dim_hidden)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout()
+        self.layer_hidden = nn.Linear(dim_hidden, dim_out)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = x.view(-1, x.shape[1]*x.shape[-2]*x.shape[-1])
+        x = self.layer_input(x)
+        x = self.dropout(x)
+        x = self.relu(x)
+        x = self.layer_hidden(x)
+        return self.softmax(x)
+
+
+class CNNMnist(nn.Module):
+    def __init__(self, args):
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(args.num_channels, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, args.num_classes)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+
+class CNNFashion_Mnist(nn.Module):
+    def __init__(self, args):
+        super(CNNFashion_Mnist, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2))
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(6, 12, kernel_size=5),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2))
+        self.fc = nn.Sequential(
+            nn.Linear(12 * 4 * 4, 120),
+            nn.ReLU(),
+            nn.Linear(120, 60),
+            nn.ReLU(),
+        )
+        self.out = nn.Linear(60,10)
+
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        x = self.out(x)
+        return x
+
+
+class CNNCifar(nn.Module):
+    def __init__(self, args):
+        super(CNNCifar, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 3)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, args.num_classes)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        # x = x.view(-1, 16 * 5 * 5)
+        x = torch.flatten(x,1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        #return F.log_softmax(x, dim=1)
+        return x
+    
+class modelC(nn.Module):
+    def __init__(self, input_size, n_classes=10, **kwargs):
+        super(AllConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(input_size, 96, 3, padding=1)
+        self.conv2 = nn.Conv2d(96, 96, 3, padding=1)
+        self.conv3 = nn.Conv2d(96, 96, 3, padding=1, stride=2)
+        self.conv4 = nn.Conv2d(96, 192, 3, padding=1)
+        self.conv5 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv6 = nn.Conv2d(192, 192, 3, padding=1, stride=2)
+        self.conv7 = nn.Conv2d(192, 192, 3, padding=1)
+        self.conv8 = nn.Conv2d(192, 192, 1)
+
+        self.class_conv = nn.Conv2d(192, n_classes, 1)
+
+
+    def forward(self, x):
+        x_drop = F.dropout(x, .2)
+        conv1_out = F.relu(self.conv1(x_drop))
+        conv2_out = F.relu(self.conv2(conv1_out))
+        conv3_out = F.relu(self.conv3(conv2_out))
+        conv3_out_drop = F.dropout(conv3_out, .5)
+        conv4_out = F.relu(self.conv4(conv3_out_drop))
+        conv5_out = F.relu(self.conv5(conv4_out))
+        conv6_out = F.relu(self.conv6(conv5_out))
+        conv6_out_drop = F.dropout(conv6_out, .5)
+        conv7_out = F.relu(self.conv7(conv6_out_drop))
+        conv8_out = F.relu(self.conv8(conv7_out))
+
+        class_out = F.relu(self.class_conv(conv8_out))
+        pool_out = F.adaptive_avg_pool2d(class_out, 1)
+        pool_out.squeeze_(-1)
+        pool_out.squeeze_(-1)
+        return pool_out
+ 
+class VGG(nn.Module):
+    def __init__(self,args):
+        super(VGG, self).__init__()
+        self.inputs = [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+        self.classifier = nn.Linear(512, 10)
+        self.features = self._make_layers(self.inputs)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+    def _make_layers(self, inputs):
+        layers = []
+        in_channels = 3
+        for x in inputs:
+            if x == 'M':
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
